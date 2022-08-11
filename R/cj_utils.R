40af50cac859f850d327e115278302ded34b1b8d
CJ <candido.mendesjr@gmail.com>
2022-08-11 13:00:55 -0500

    initial commit. add readme and R utils

---
+++ R/cj_utils.R
@@ -0,0 +1,34 @@
+name_cleaner <- function(df){
+  names(df) <- tolower(names(df))
+  names(df) <- str_replace_all(names(df), "\\r\\n", "")
+  names(df) <- str_replace_all(names(df), "\\\"", "")
+  names(df) <- str_replace_all(names(df), "\\.$", "")
+  names(df) <- str_replace_all(names(df), "\\.", "_")
+  names(df) <- str_replace_all(names(df), "\\-", "_")
+  names(df) <- str_replace_all(names(df), "\\%", "pct")
+  names(df) <- str_replace_all(names(df), "\\?$", "")
+  names(df) <- str_replace_all(names(df), "\\+", "")
+  names(df) <- str_replace_all(names(df), "\\#", "")
+  names(df) <- str_replace_all(names(df), "\\(|\\)|,", "")
+  names(df) <- str_replace_all(names(df), "\\/", "_")
+  names(df) <- str_replace_all(names(df), "\\s", "_")
+  names(df) <- str_replace_all(names(df), "_+", "_")
+  names(df) <- str_replace_all(names(df), "_$", "")
+  names(df) <- str_replace_all(names(df), "^_", "")
+  return(df)
+}
+
+excel_pct <- function(x) {
+  z <- as.numeric(format(round(x, 3), nsmall = 2))
+  return(z)
+}
+
+get_file <- function(file_path, filename_pattern = ".xlsx$|.xls$") {
+  files <- file.info(list.files(path = file_path, 
+                                pattern = filename_pattern, 
+                                full.names = TRUE)) %>%
+    rownames_to_column(var = "filename") %>%
+    arrange(desc(ctime)) %>%
+    mutate(filename = tolower(filename))
+  return(files[1, 1])
+}
