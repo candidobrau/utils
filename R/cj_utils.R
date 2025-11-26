b92746e6ed895f2eebc84c3a0dbaf1eeb08f7d11
CJ <candido.mendesjr@gmail.com>
2025-11-26 11:12:27 -0600

    change ctime to mtime in get_file

--- R/cj_utils.R
+++ R/cj_utils.R
@@ -34,7 +34,29 @@ get_file <- function(file_path, filename_pattern = ".xlsx$|.xls$") {
                                 pattern = filename_pattern, 
                                 full.names = TRUE)) %>%
     rownames_to_column(var = "filename") %>%
-    arrange(desc(ctime)) %>%
+    arrange(desc(mtime)) %>%
     mutate(filename = tolower(filename))
   return(files[1, 1])
+}
+
+
+read_two_row_headers <- function(path, 
+                                 sheet = 1, 
+                                 sep = " ") {
+  # Read only the first two rows, no column names
+  hdr <- read_excel(path, 
+                    sheet = sheet, 
+                    col_names = FALSE, 
+                    n_max = 2)
+  
+  # Combine row 1 and row 2 into single header strings
+  combined <- hdr %>%
+    summarise(across(everything(), ~ str_c(., collapse = sep))) |>
+    unlist(use.names = FALSE)
+  
+  # Clean to snake_case
+  cleaned <- combined |>
+    name_cleaner()
+  
+  return(cleaned)
 }
